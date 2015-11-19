1cfe10f962d572beaa883fd6254d0df151c1b62c
Christopher Sexton <github@codeography.com>
2015-11-19 15:14:54 -0500

    update serhex fingerprint

--- Formula/serhex.rb
+++ Formula/serhex.rb
@@ -4,7 +4,7 @@ class Serhex < Formula
   homepage 'https://github.com/RadiusNetworks/serhex'
   head 'git@github.com:RadiusNetworks/serhex.git'
   url 'https://github.com/RadiusNetworks/serhex/archive/v0.1.tar.gz'
-  md5 '15768dc46244384c2cb3bfcd46c7e0c5'
+  sha256 '2e8d3f4c36977bd671094bbd1d73d29646d583cc98cb7d57c0f0021e48e6a583'
   version '0.1'
 
   def install
